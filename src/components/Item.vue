145e7f7451e4f1e37092d706c311e54fe8ad8791
Mariano <marianomarchionna8@gmail.com>
2021-09-29 11:21:09 +0200

    Generi e migliorie cast

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -5,15 +5,36 @@
                 <img v-if="item.poster_path!=null" :src="`https://image.tmdb.org/t/p/w342${item.poster_path}`" :alt="item.title">
                 <img v-else src="../assets/img/error.jpg" alt="ERRORE">
                 <div class="description">
+                    <!-- titolo -->
                     <span><strong>Titolo:</strong> {{ item.title || item.name }}</span>
+                    <!-- titolo originale -->
                     <span v-if="item.title!=item.original_title || item.name!=item.original_name"><strong>Titolo originale:</strong> {{ item.original_title || item.original_name }}</span>
+                    <!-- voto -->
                     <span v-if="item.vote_average!=0"><strong>Voto: </strong><i class="fas fa-star" v-for="(star, index) in getVote(item.vote_average)" :key="index"></i></span>
                     <span v-else><strong>Voto non disponibile</strong></span>
+                    <!-- overview -->
                     <p v-if="item.overview!=''" class="overview"><strong>Overview:</strong> {{ item.overview }} </p>
                     <span v-else><strong>Overview non disponibile</strong></span>
+                    <!-- lingua -->
                     <span class="flag"><strong>Lingua originale:</strong><country-flag :country="getFlag(item.original_language)"/></span>
-                    <span v-if="item.name === undefined"><strong>Cast: </strong><Cast :type='movie' :id="item.id" /></span>
-                    <span v-else-if="item.title === undefined"><strong>Cast: </strong><Cast :type='tv' :id="item.id" /></span>
+                    <!-- cast -->
+                    <span v-if="item.name === undefined">
+                        <strong>Cast: </strong>
+                            <p class="inline" v-for="(actor, index) in getCast(movie, item.id)" :key="index">{{actor}}.  </p>
+                    </span>
+                    <span v-else-if="item.title === undefined">
+                        <strong>Cast: </strong>
+                            <p class="inline" v-for="(actor, index) in getCast(tv, item.id)" :key="index">{{actor}}.  </p>
+                    </span>
+                    <!-- genere -->
+                    <span v-if="item.name === undefined">
+                        <strong>Genere: </strong>
+                        <p class="inline" v-for=" (genre, index) in item.genre_ids" :key="index">{{checkGenre(movie, genre)}}.  </p>
+                    </span>
+                    <span v-else-if="item.title === undefined">
+                        <strong>Genere: </strong>
+                        <p class="inline" v-for=" (genre, index) in item.genre_ids" :key="index">{{checkGenre(tv, genre)}}.  </p>
+                    </span>
                 </div>
             </div>
         </div>
@@ -22,19 +43,21 @@
 
 <script>
 import CountryFlag from 'vue-country-flag'
-import Cast from "./Cast.vue";
+import axios from "axios";
 
 export default {
   name: 'Item',
   props: ['item'],
   components: {
-    CountryFlag,
-    Cast
+    CountryFlag
   },
   data() {
     return {
         movie: 'movie',
-        tv: 'tv'
+        tv: 'tv',
+        cast: [],
+        movieId: [],
+        tvId:[]
     }
   },
   methods:{
@@ -50,8 +73,76 @@ export default {
             vote = vote/2;
             vote = Math.ceil(vote);
             return vote;
+        },
+        getCast(type, id) {
+            if(type === 'movie'){
+                axios
+                .get(
+                    "https://api.themoviedb.org/3/movie/" +
+                    id +
+                    "/credits?api_key=8aeadd2568b18f3ed669ace3260041bb"
+                )
+                .then((res) => {
+                    let castArray = res.data.cast;
+                    castArray.forEach((element) => {
+                        if (this.cast.length < 5) {
+                        this.cast.push(element.name);
+                        }
+                    });
+                });
+                return this.cast;
+            }else if(type === 'tv'){
+                axios
+                .get(
+                    "https://api.themoviedb.org/3/tv/" +
+                    id +
+                    "/credits?api_key=8aeadd2568b18f3ed669ace3260041bb"
+                )
+                .then((res) => {
+                    let castArray = res.data.cast;
+                    castArray.forEach((element) => {
+                        if (this.cast.length < 5) {
+                        this.cast.push(element.name);
+                        }
+                    });
+                });
+                return this.cast;
+            }
+        },
+        APIMovieGenre() {
+            axios
+            .get('https://api.themoviedb.org/3/genre/movie/list?api_key=8aeadd2568b18f3ed669ace3260041bb')
+            .then(res => {
+                this.movieId = res.data.genres;
+                return this.movieId
+            })
+        },
+        APITvGenre() {
+            axios
+            .get('https://api.themoviedb.org/3/genre/tv/list?api_key=8aeadd2568b18f3ed669ace3260041bb')
+            .then(res => {
+                this.tvId = res.data.genres;
+                return this.tvId
+            })
+        },
+        checkGenre(type, id) {
+            if( type === 'movie'){
+                for(let i = 0; i < this.movieId.length; i++){
+                if(this.movieId[i].id === id)
+                    return this.movieId[i].name
+                }
+            }else{
+                for(let i = 0; i < this.tvId.length; i++){
+                if(this.tvId[i].id === id)
+                    return this.tvId[i].name
+                }
+            }
         }
-  }
+    },
+    created() {
+        this.APIMovieGenre(),
+        this.APITvGenre()
+    },
 }
 </script>
 
@@ -96,7 +187,7 @@ export default {
             }
             .overview {
                 width: 100%;
-                height: 30%;
+                max-height: 18%;
                 overflow: hidden;
                 text-overflow: ellipsis;
             }
@@ -106,6 +197,9 @@ export default {
             }
         }
     }
+}
+.inline {
+    display: inline;
 }
 .card:hover .card-item {
   transform: rotateY(180deg);
